9a494a9e85137574a4b18f65093dd281c665fa2d
Cero-Kurn <147090016+Cero-Kurn@users.noreply.github.com>
2026-04-23 15:01:19 -0600

    Refactor renderClimateAnomalies function
    export function renderClimateAnomalies(regions, decoded) { ... }
    function renderClimateAnomalies(regions, decoded) {

--- world-seed-system/modules/climate/climateAnomalies.js
+++ world-seed-system/modules/climate/climateAnomalies.js
@@ -1,5 +1,5 @@
 // ---CLIMATE ANOMALIES ---
-export function renderClimateAnomalies(regions, decoded) ///{ ... } //function renderClimateAnomalies(regions, decoded) {
+export function renderClimateAnomalies(regions, decoded) {
   const container = document.getElementById("climateAnomalies");
   const lat = decoded.lm.primary.toLowerCase();
   const wind = decoded.we.primary.toLowerCase();
